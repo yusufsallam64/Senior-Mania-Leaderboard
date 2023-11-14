7540b9e0b798404bb71fc355af36800dc03ad823
Safa-Karagoz <78101103+Safa-Karagoz@users.noreply.github.com>
2023-11-13 19:10:40 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LeaderBoard from './LeaderBoard';
+
+function App() {
+  return (
+    <div className="App">
+      <LeaderBoard />
+    </div>
+  );
+}
+
+export default App;
